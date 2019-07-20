324e6dcc3f5fef3ff7274964fa6d77e2af849f8d
ChAgata <agata.chrin.g23@gmail.com>
2019-07-20 17:37:13 +0100

    Add project files.

---
+++ Calculator/Dzialania.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Dzialania
+    {
+        public int x;
+        public int y;
+        public int dodawanie()
+        {
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            int wynik = x + y;
+            return wynik;
+        }
+        public int odejmowanie()
+        {
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            int wynik = x - y;
+            return wynik;
+        }
+        public int mnozenie()
+        {
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            int wynik = x * y;
+            return wynik;
+        }
+        public int dzielenie()
+        {
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            int wynik = x / y;
+            return wynik;
+        }
+        public int dzielenieModulo()
+        {
+            x = Int32.Parse(Console.ReadLine());
+            y = Int32.Parse(Console.ReadLine());
+            int wynik = x % y;
+            return wynik;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            do
+            {
+                int dzialanie = Menu();
+                Dzialania klasa = new Dzialania();
+                switch (dzialanie)
+                {
+                    case 1:
+                        Console.WriteLine("Wybrałeś Dodawanie. Wprowadz dwie liczby.");
+                        int wynikDodawania = klasa.dodawanie();
+                        Console.WriteLine(wynikDodawania);
+                        break;
+                    case 2:
+                        Console.WriteLine("Wybrałeś Odejmowanie Wprowadz dwie liczby.");
+                        int wynikOdejmowania = klasa.odejmowanie();
+                        Console.WriteLine(wynikOdejmowania);
+                        break;
+                    case 3:
+                        Console.WriteLine("Wybrałeś Mnożenie");
+                        int wynikMnozenia = klasa.mnozenie();
+                        Console.WriteLine(wynikMnozenia);
+                        break;
+                    case 4:
+                        Console.WriteLine("Wybrałeś Dzielenie");
+                        int wynikDzielenia = klasa.dzielenie();
+                        Console.WriteLine(wynikDzielenia);
+                        break;
+                    case 5:
+                        Console.WriteLine("Wybrałeś Dzielenie Modulo");
+                        int wynikModulo = klasa.dzielenieModulo();
+                        Console.WriteLine(wynikModulo);
+                        break;
+                    default:
+                        Console.WriteLine("Wybrałeś numer spoza zakresu. Wybierz jeszcze raz.");
+                        break;
+                }               
+                Console.ReadLine();
+            }
+            while (true);  
+        }
+        private static int Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Wybierz numer działania:\n 1. Dodawanie \n 2. Odejmowanie \n 3. Mnożenie \n 4. Dzielenie \n 5. Modulo");
+            int dzialanie = Int32.Parse(Console.ReadLine());
+            return dzialanie;
+        }
+    }
+}
