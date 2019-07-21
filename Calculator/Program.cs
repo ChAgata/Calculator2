0a4e7d53b9e86d41af8d6a1e749f221f0d97fe9c
ChAgata <agata.chrin.g23@gmail.com>
2019-07-21 13:46:27 +0100

    zmiana

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,7 +18,7 @@ namespace Calculator
                 switch (dzialanie)
                 {
                     case 1:
-                        Console.WriteLine("Wybrałeś Dodawanie. WWprowadddz dwie liczby.");
+                        Console.WriteLine("Wybrałeś Dodawanie. Wprowadddz dwie liczby.");
                         int wynikDodawania = klasa.dodawanie();
                         Console.WriteLine(wynikDodawania);
                         break;
